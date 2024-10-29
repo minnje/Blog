f05bfea8edfb2bf9e34d5cbca226224dbfb55fa0
minnje <167839981+minnje@users.noreply.github.com>
2024-10-30 00:28:24 +0900

    init: 프로젝트 초기 생성

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./index.css";
+
+function App() {
+     return <h1 className="text-yellow-500">hello</h1>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+     base: "/",
+     server: {
+          host: "localhost",
+          port: 3000,
+          open: false,
+     },
+     plugins: [react()],
+});
