5c741c41afd9916c2a46f2eeaffff030427c75f1
minnje <167839981+minnje@users.noreply.github.com>
2024-12-04 18:48:54 +0900

    오류와 스타일 디테일 수정

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -5,193 +5,198 @@ import { useEffect, useState } from 'react';
 import { useEditQuery } from '../utils/api';
 import { Helmet } from 'react-helmet-async';
 import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 import { IDatas } from '../types';
 
 function Edit() {
-   const navigate = useNavigate();
-   const { editId } = useParams();
-   const { data } = useEditQuery();
-   const typedData = data as IDatas;
-   const collectionName = localStorage.getItem('collectionName');
+    const navigate = useNavigate();
+    const { editId } = useParams();
+    const { data } = useEditQuery();
+    const typedData = data as IDatas;
+    const collectionName = localStorage.getItem('collectionName');
 
-   const {
-      register,
-      handleSubmit,
-      setValue,
-      control,
-      formState: { errors },
-   } = useForm();
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        control,
+        formState: { errors },
+    } = useForm();
 
-   const [preview, setPreview] = useState<string | null>(null);
+    const [preview, setPreview] = useState<string | null>(null);
 
-   useEffect(() => {
-      if (typedData?.img && typedData?.img.length !== 0) {
-         setValue('img', typedData?.img);
-         setPreview(
+    if (typedData?.img && typedData?.img.length !== 0) {
+        setValue('img', typedData?.img);
+        setPreview(
             `${import.meta.env.VITE_PB_API}/files/${typedData?.collectionName}/${typedData?.id}/${typedData?.img}`
-         );
-      }
-      setValue('title', typedData?.title);
-      setValue('content', typedData?.content);
-   }, [setValue]);
+        );
+    }
+    setValue('title', typedData?.title);
+    setValue('content', typedData?.content);
 
-   async function onValid(typedData: any) {
-      if (errors === null) {
-         return;
-      }
-      if (collectionName === 'Memo') {
-         const { title, content, img } = typedData;
+    async function onValid(typedData: any) {
+        if (errors === null) {
+            return;
+        }
+        if (collectionName === 'Memo') {
+            const { title, content, img } = typedData;
 
-         if (img && img[0]) {
-            const formData = new FormData();
+            if (img && img.length > 0) {
+                const formData = new FormData();
 
-            for (let file of img) {
-               formData.append('img', file);
+                for (let file of img) {
+                    formData.append('img', file);
+                }
+
+                formData.append('content', content);
+                formData.append('title', title);
+
+                try {
+                    await pb.collection('Memo').update(editId + '', formData);
+                } catch (error) {
+                    console.error('File upload failed', error);
+                }
+            } else {
+                await pb.collection('Memo').update(editId + '', {
+                    title,
+                    content,
+                });
             }
+            navigate('/memo');
+        } else if (collectionName === 'Troubleshooting') {
+            const { title, content, img } = typedData;
 
-            formData.append('content', content);
-            formData.append('title', title);
+            if (img && img[0]) {
+                const formData = new FormData();
 
-            try {
-               await pb.collection('Memo').update(editId + '', formData);
-            } catch (error) {
-               console.error('File upload failed', error);
+                for (let file of img) {
+                    formData.append('img', file);
+                }
+
+                formData.append('content', content);
+                formData.append('title', title);
+
+                try {
+                    await pb
+                        .collection('Troubleshooting')
+                        .update(editId + '', formData);
+                } catch (error) {
+                    console.error('File upload failed', error);
+                }
+            } else {
+                await pb.collection('Troubleshooting').update(editId + '', {
+                    title,
+                    content,
+                });
             }
-         } else {
-            await pb.collection('Memo').update(editId + '', {
-               title,
-               content,
-            });
-         }
-         navigate('/memo');
-      } else if (collectionName === 'Troubleshooting') {
-         const { title, content, img } = typedData;
+            navigate('/troubleshooting');
+        }
+    }
 
-         if (img && img[0]) {
-            const formData = new FormData();
+    const handlePreview = (e: any) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        if (file) {
+            reader.onload = function () {
+                setPreview(reader.result as string);
+            };
 
-            for (let file of img) {
-               formData.append('img', file);
-            }
+            reader.onerror = function (error) {
+                console.error('File preview error:', error);
+            };
 
-            formData.append('content', content);
-            formData.append('title', title);
+            reader.readAsDataURL(file);
+        }
+    };
 
-            try {
-               await pb
-                  .collection('Troubleshooting')
-                  .update(editId + '', formData);
-            } catch (error) {
-               console.error('File upload failed', error);
-            }
-         } else {
-            await pb.collection('Troubleshooting').update(editId + '', {
-               title,
-               content,
-            });
-         }
-         navigate('/troubleshooting');
-      }
-   }
+    return (
+        <>
+            <Helmet>
+                <title>글 수정 | minje blog</title>
+                <script
+                    src="https://kit.fontawesome.com/7e06fe1b34.js"
+                    crossOrigin="anonymous"
+                ></script>
+            </Helmet>
+            <div className="flex w-full flex-col rounded-xl bg-white p-3">
+                <h1 className="input-basic mb-3 text-center">글 수정</h1>
+                <form
+                    className="flex h-full flex-col justify-between gap-2"
+                    onSubmit={handleSubmit(onValid)}
+                >
+                    <input
+                        className="focus-custom-slim input-basic"
+                        {...register('title', {
+                            required: '제목을 입력해 주세요',
+                            maxLength: {
+                                value: 100,
+                                message: '제목은 100자 이내로 입력해 주세요',
+                            },
+                        })}
+                        placeholder="제목"
+                    />
+                    <span>{JSON.stringify(errors.title?.message)}</span>
+                    <label
+                        htmlFor="imageInput"
+                        className="focus-custom-slim input-basic mb-1 flex items-center gap-1"
+                        tabIndex={0}
+                    >
+                        <i className="fa-regular fa-file-image text-xs"></i>
+                        이미지 선택
+                        <input
+                            type="file"
+                            accept="image/*"
+                            {...register('img', { onChange: handlePreview })}
+                            className="hidden"
+                            multiple
+                            id="imageInput"
+                        />
+                    </label>
+                    {preview && (
+                        <img
+                            src={preview + ''}
+                            alt="이미지 미리보기"
+                            className="w-48"
+                        />
+                    )}
+                    <Controller
+                        name="content"
+                        control={control}
+                        defaultValue={typedData?.content}
+                        rules={{ required: '내용은 필수입니다.' }}
+                        render={({ field }) => (
+                            <ReactQuill
+                                className="flex h-full flex-col"
+                                value={field.value}
+                                onChange={(value) => {
+                                    field.onChange(value);
+                                }}
+                                theme="snow"
+                            />
+                        )}
+                    />
+                    <span>{JSON.stringify(errors.content?.message)}</span>
+                    <div className="flex w-full flex-row justify-between">
+                        <button
+                            className="focus-custom-slim input-basic w-full font-medium"
+                            onClick={() => navigate(-1)}
+                            type="button"
+                        >
+                            취소
+                        </button>
 
-   const handlePreview = (e: any) => {
-      const file = e.target.files[0];
-      const reader = new FileReader();
-      if (file) {
-         reader.onload = function () {
-            setPreview(reader.result + '');
-         };
-
-         reader.onerror = function (error) {
-            console.error('File preview error:', error);
-         };
-
-         reader.readAsDataURL(file);
-      }
-   };
-
-   return (
-      <>
-         <Helmet>
-            <title>글 수정 | minje blog</title>
-            <script
-               src="https://kit.fontawesome.com/7e06fe1b34.js"
-               crossOrigin="anonymous"
-            ></script>
-         </Helmet>
-         <div className="m-4 flex w-full flex-col gap-1">
-            <h1 className="mb-2 ml-1 text-sm">글 수정</h1>
-            <form
-               className="flex flex-col gap-2"
-               onSubmit={handleSubmit(onValid)}
-            >
-               <input
-                  className="focus-custom-slim input-basic"
-                  {...register('title', {
-                     required: '제목을 입력해 주세요',
-                     maxLength: {
-                        value: 100,
-                        message: '제목은 100자 이내로 입력해 주세요',
-                     },
-                  })}
-                  placeholder="제목"
-               />
-               <span>{JSON.stringify(errors.title?.message)}</span>
-               <label
-                  htmlFor="imageInput"
-                  className="focus-custom-slim input-basic text-center"
-                  tabIndex={0}
-               >
-                  <i className="fa-regular fa-file-image text-xs"></i> 이미지
-                  선택
-                  <input
-                     type="file"
-                     accept="image/*"
-                     {...register('img', { onChange: handlePreview })}
-                     className="hidden"
-                     multiple
-                     id="imageInput"
-                  />
-               </label>
-               {preview && (
-                  <img src={preview + ''} alt={`미리보기`} className="w-3/4" />
-               )}
-               <Controller
-                  name="content"
-                  control={control}
-                  defaultValue={''}
-                  rules={{ required: '내용은 필수입니다.' }}
-                  render={({ field }) => (
-                     <ReactQuill
-                        value={field.value}
-                        onChange={(value) => {
-                           field.onChange(value);
-                        }}
-                        theme="snow"
-                     />
-                  )}
-               />{' '}
-               <span>{JSON.stringify(errors.content?.message)}</span>
-               <div className="mt-10 flex w-full flex-row justify-between">
-                  <button
-                     type="submit"
-                     className="focus-custom-slim input-basic w-full font-medium"
-                  >
-                     완료
-                  </button>
-
-                  <button
-                     className="focus-custom-slim input-basic w-full font-medium"
-                     onClick={() => navigate(-1)}
-                     type="button"
-                  >
-                     취소
-                  </button>
-               </div>
-            </form>
-         </div>
-      </>
-   );
+                        <button
+                            type="submit"
+                            className="focus-custom-slim input-basic w-full font-medium"
+                        >
+                            완료
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default Edit;
--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -7,180 +7,181 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 function Write() {
-   const navigate = useNavigate();
-   const location = useLocation();
-   const {
-      register,
-      handleSubmit,
-      control,
-      formState: { errors },
-   } = useForm();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm();
 
-   const [preview, setPreview] = useState<string | null>(null);
+    const [preview, setPreview] = useState<string | null>(null);
 
-   const onValid = async (data: any) => {
-      if (errors === null) {
-         return;
-      }
-      if (location.pathname.includes('memo')) {
-         const { title, content, img } = data;
+    const onValid = async (data: any) => {
+        if (errors === null) {
+            return;
+        }
+        if (location.pathname.includes('memo')) {
+            const { title, content, img } = data;
 
-         if (img && img[0]) {
-            const formData = new FormData();
+            if (img && img.length > 0) {
+                const formData = new FormData();
 
-            for (let file of img) {
-               formData.append('img', file);
+                for (let file of img) {
+                    formData.append('img', file);
+                }
+
+                formData.append('content', content);
+
+                formData.append('title', title);
+
+                try {
+                    await pb.collection('Memo').create(formData);
+                } catch (error) {
+                    console.error('File upload failed', error);
+                }
+            } else {
+                await pb.collection('Memo').create({
+                    title,
+                    content,
+                });
             }
+            navigate('/memo');
+        } else if (location.pathname.includes('troubleshooting')) {
+            const { title, content, img } = data;
 
-            formData.append('content', content);
+            if (img && img[0]) {
+                const formData = new FormData();
 
-            formData.append('title', title);
+                for (let file of img) {
+                    formData.append('img', file);
+                }
 
-            try {
-               await pb.collection('Memo').create(formData);
-            } catch (error) {
-               console.error('File upload failed', error);
+                formData.append('content', content);
+                formData.append('title', title);
+
+                try {
+                    await pb.collection('Troubleshooting').create(formData);
+                } catch (error) {
+                    console.error('File upload failed', error);
+                }
+            } else {
+                await pb.collection('Troubleshooting').create({
+                    title,
+                    content,
+                });
             }
-         } else {
-            await pb.collection('Memo').create({
-               title,
-               content,
-            });
-         }
-         navigate('/memo');
-      } else if (location.pathname.includes('troubleshooting')) {
-         const { title, content, img } = data;
+            navigate('/troubleshooting');
+        }
+    };
 
-         if (img && img[0]) {
-            const formData = new FormData();
+    const handlePreview = (e: any) => {
+        const reader = new FileReader();
+        const file = e.target.files[0];
+        if (file) {
+            reader.onload = function () {
+                setPreview(reader.result as string);
+            };
 
-            for (let file of img) {
-               formData.append('img', file);
-            }
+            reader.onerror = function (error) {
+                console.error('File preview error:', error);
+            };
 
-            formData.append('content', content);
-            formData.append('title', title);
+            reader.readAsDataURL(file);
+        }
+    };
 
-            try {
-               await pb.collection('Troubleshooting').create(formData);
-            } catch (error) {
-               console.error('File upload failed', error);
-            }
-         } else {
-            await pb.collection('Troubleshooting').create({
-               title,
-               content,
-            });
-         }
-         navigate('/troubleshooting');
-      }
-   };
+    return (
+        <>
+            <Helmet>
+                <title>글쓰기 | minje blog</title>
+                <script
+                    src="https://kit.fontawesome.com/7e06fe1b34.js"
+                    crossOrigin="anonymous"
+                ></script>
+            </Helmet>
+            <div className="flex w-full flex-col rounded-xl bg-white p-3">
+                <h1 className="input-basic mb-3 text-center">글쓰기</h1>
+                <form
+                    className="flex h-full flex-col justify-between gap-2"
+                    onSubmit={handleSubmit(onValid)}
+                >
+                    <input
+                        className="focus-custom-slim input-basic"
+                        {...register('title', {
+                            required: '제목을 입력해 주세요',
+                            maxLength: {
+                                value: 100,
+                                message: '제목은 100자 이내로 입력해 주세요',
+                            },
+                        })}
+                        placeholder="제목"
+                    />
+                    <span>{JSON.stringify(errors.title?.message)}</span>
 
-   const handlePreview = (e: any) => {
-      const reader = new FileReader();
-      const file = e.target.files[0];
-      if (file) {
-         reader.onload = function () {
-            setPreview(reader.result + '');
-         };
+                    <label
+                        htmlFor="img"
+                        className="focus-custom-slim input-basic mb-1 flex items-center gap-1"
+                        tabIndex={0}
+                    >
+                        <i className="fa-regular fa-file-image text-xs"></i>
+                        이미지 선택
+                        <input
+                            type="file"
+                            accept="image/*"
+                            {...register('img', { onChange: handlePreview })}
+                            className="hidden"
+                            multiple
+                            id="img"
+                        />
+                    </label>
 
-         reader.onerror = function (error) {
-            console.error('File preview error:', error);
-         };
+                    {preview && (
+                        <img
+                            src={preview + ''}
+                            alt="이미지 미리보기"
+                            className="w-48"
+                        />
+                    )}
 
-         reader.readAsDataURL(file);
-      }
-   };
+                    <Controller
+                        name="content"
+                        control={control}
+                        defaultValue={''}
+                        rules={{ required: '내용은 필수입니다.' }}
+                        render={({ field }) => (
+                            <ReactQuill
+                                className="flex h-full flex-col"
+                                value={field.value}
+                                onChange={(value) => {
+                                    field.onChange(value);
+                                }}
+                                theme="snow"
+                            />
+                        )}
+                    />
+                    <span>{JSON.stringify(errors.content?.message)}</span>
 
-   return (
-      <>
-         <Helmet>
-            <title>글쓰기 | minje blog</title>
-            <script
-               src="https://kit.fontawesome.com/7e06fe1b34.js"
-               crossOrigin="anonymous"
-            ></script>
-         </Helmet>
-         <div className="m-4 flex w-full flex-col gap-1">
-            <h1 className="mb-2 ml-1 text-sm">글쓰기</h1>
-            <form
-               className="flex flex-col gap-2"
-               onSubmit={handleSubmit(onValid)}
-            >
-               <input
-                  className="focus-custom-slim input-basic"
-                  {...register('title', {
-                     required: '제목을 입력해 주세요',
-                     maxLength: {
-                        value: 100,
-                        message: '제목은 100자 이내로 입력해 주세요',
-                     },
-                  })}
-                  placeholder="제목"
-               />
-               <span>{JSON.stringify(errors.title?.message)}</span>
-
-               <label
-                  htmlFor="img"
-                  className="focus-custom-slim input-basic text-center"
-                  tabIndex={0}
-               >
-                  <i className="fa-regular fa-file-image text-xs"></i> 이미지
-                  선택
-                  <input
-                     type="file"
-                     accept="image/*"
-                     {...register('img', { onChange: handlePreview })}
-                     className="hidden"
-                     multiple
-                     id="img"
-                  />
-               </label>
-
-               {preview && (
-                  <img
-                     src={preview + ''}
-                     alt={`미리보기`}
-                     className="h-32 w-32"
-                  />
-               )}
-
-               <Controller
-                  name="content"
-                  control={control}
-                  defaultValue={''}
-                  rules={{ required: '내용은 필수입니다.' }}
-                  render={({ field }) => (
-                     <ReactQuill
-                        value={field.value}
-                        onChange={(value) => {
-                           field.onChange(value);
-                        }}
-                        theme="snow"
-                     />
-                  )}
-               />
-               <span>{JSON.stringify(errors.content?.message)}</span>
-
-               <div className="mt-10 flex w-full flex-row justify-between">
-                  <button
-                     className="focus-custom-slim input-basic w-full font-medium"
-                     onClick={() => navigate(-1)}
-                     type="button"
-                  >
-                     취소
-                  </button>
-                  <button
-                     type="submit"
-                     className="focus-custom-slim input-basic w-full font-medium"
-                  >
-                     완료
-                  </button>
-               </div>
-            </form>
-         </div>
-      </>
-   );
+                    <div className="flex w-full flex-row justify-between">
+                        <button
+                            className="focus-custom-slim input-basic w-full font-medium"
+                            onClick={() => navigate(-1)}
+                            type="button"
+                        >
+                            취소
+                        </button>
+                        <button
+                            type="submit"
+                            className="focus-custom-slim input-basic w-full font-medium"
+                        >
+                            완료
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default Write;
