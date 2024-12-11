ccb54c9130156a0e5ea71ae84e0e6bcff720f19d
minnje <167839981+minnje@users.noreply.github.com>
2024-12-12 04:38:26 +0900

    vite 버전 업데이트 및 버그찾기

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,16 @@ export default defineConfig({
     build: {
         outDir: 'dist',
         rollupOptions: {
+            plugins: [
+                {
+                    name: 'log-env-vars',
+                    buildStart() {
+                        // Vite 빌드 시작 시 환경 변수를 로그로 출력
+                        console.log('VITE_PB_URL:', process.env.VITE_PB_URL);
+                        console.log('VITE_PB_API:', process.env.VITE_PB_API);
+                    },
+                },
+            ],
             output: {
                 manualChunks(id) {
                     if (id.includes('node_modules')) {
@@ -37,6 +47,8 @@ export default defineConfig({
     base: '/',
     define: {
         'process.env': process.env,
+        'process.env.VITE_PB_URL': JSON.stringify(process.env.VITE_PB_URL),
+        'process.env.VITE_PB_API': JSON.stringify(process.env.VITE_PB_API),
     },
     server: {
         host: 'localhost',
