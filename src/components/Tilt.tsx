a6f4959f692ba2672c2eb80cdc4df30fae34714f
minnje <167839981+minnje@users.noreply.github.com>
2024-12-12 07:34:39 +0900

    Tilt 컴포넌트 코드 수정

--- src/components/Tilt.tsx
+++ src/components/Tilt.tsx
@@ -2,35 +2,39 @@ import { useEffect, useRef, HTMLAttributes } from 'react';
 import VanillaTilt from 'vanilla-tilt';
 
 declare global {
-   interface HTMLDivElement {
-      vanillaTilt?: {
-         destroy: () => void;
-      };
-   }
+    interface HTMLDivElement {
+        vanillaTilt?: {
+            destroy: () => void;
+        };
+    }
 }
 
 type VanillaTiltOptions = Parameters<typeof VanillaTilt.init>[1];
 
 interface TiltProps extends HTMLAttributes<HTMLDivElement> {
-   options: VanillaTiltOptions;
+    options: VanillaTiltOptions;
 }
 
-const Tilt: React.FC<TiltProps> = ({ options, ...props }) => {
-   const tiltRef = useRef<HTMLDivElement | null>(null);
+const Tilt: React.FC<TiltProps> = ({ options, children, ...props }) => {
+    const tiltRef = useRef<HTMLDivElement | null>(null);
 
-   useEffect(() => {
-      if (tiltRef.current) {
-         VanillaTilt.init(tiltRef.current, options);
-      }
+    useEffect(() => {
+        if (tiltRef.current) {
+            VanillaTilt.init(tiltRef.current, options);
+        }
 
-      return () => {
-         if (tiltRef.current && tiltRef.current.vanillaTilt) {
-            tiltRef.current.vanillaTilt.destroy();
-         }
-      };
-   }, [options]);
+        return () => {
+            if (tiltRef.current && tiltRef.current.vanillaTilt) {
+                tiltRef.current.vanillaTilt.destroy();
+            }
+        };
+    }, [options]);
 
-   return <div ref={tiltRef} {...props} />;
+    return (
+        <div ref={tiltRef} {...props}>
+            {children}
+        </div>
+    );
 };
 
 export default Tilt;
